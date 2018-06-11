5e0b846c69f4e77c87e9d58cb65a1e386ec2de5b
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-11 19:28:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,9 +7,8 @@ def position_taken?(board, index)
     return false
   elsif board[index] == nil
     return false
-  elsif board[index] == "X"
+  elsif board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == "O"
-    return true
+  
   end
 end
