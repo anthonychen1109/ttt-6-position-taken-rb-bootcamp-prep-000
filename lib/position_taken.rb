7a3f0e6d53d82ce63b9572f30e73491416781f1f
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-11 19:32:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,13 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-    pos = board[index]
-    case pos
-    when pos == " " || pos == "" || pos == nil
-        return false
-    when pos == "X" || pos == "O"
-        return true  
-    end
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
 end
